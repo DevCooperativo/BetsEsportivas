538db90ac0181e9b2b4029a24f6fcfaf647bd703
Nicolas Bassini <nicolasbassini@hotmail.com>
2025-05-08 21:36:46 -0300

    new: gráfico de categorias

--- betsesportivas/src/main/java/com/betsesportivas/Controllers/DashboardCategoriasController.java
+++ betsesportivas/src/main/java/com/betsesportivas/Controllers/DashboardCategoriasController.java
@@ -4,21 +4,34 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 import com.betsesportivas.App;
 import com.betsesportivas.DAO.CategoriaDAO;
+import com.betsesportivas.DAO.CompetidorDAO;
+import com.betsesportivas.DAO.ICategoriaDAO;
+import com.betsesportivas.DAO.ICompetidorDAO;
 import com.betsesportivas.DTO.CategoriaDTO;
+import com.betsesportivas.DTO.CompetidorDTO;
 import com.betsesportivas.Database.Db;
+import com.betsesportivas.Domain.Categoria;
+import com.betsesportivas.Domain.Competidor;
 import com.betsesportivas.Helpers.ColorHelper;
 import com.betsesportivas.Helpers.ErrorHelper;
 import com.betsesportivas.Helpers.ParserHelper;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
@@ -34,7 +47,8 @@ import javafx.scene.paint.Color;
 public class DashboardCategoriasController implements Initializable {
 
     private final Db Database = new Db();
-    private final CategoriaDAO categoriaDAO = new CategoriaDAO();
+    // private final CategoriaDAO categoriaDAO = new CategoriaDAO();
+    private final ICategoriaDAO<Categoria, CategoriaDTO> categoriaDAO = new CategoriaDAO();
 
     public DashboardCategoriasController() throws SQLException {
         categoriaDAO.Connect(Database.Connect());
@@ -111,10 +125,19 @@ public class DashboardCategoriasController implements Initializable {
     private MenuItem menu_jogadores_relatorio;
     // #endregion
 
+    @FXML
+    private BarChart<String, Integer> chartCategorias;
+    @FXML
+    private CategoryAxis categoryAxis;
+    @FXML
+    private NumberAxis numberAxis;
+    private ObservableList<String> observableListNomesCategorias = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
             initializeTableView();
+            populateGrafico();
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.getStackTrace();
@@ -153,6 +176,41 @@ public class DashboardCategoriasController implements Initializable {
         obvservableCategoriaDTO = FXCollections.observableArrayList(categoriaDTO);
         tblView_categorias.setItems(obvservableCategoriaDTO);
 
+    }
+
+    public void populateGrafico() throws SQLException {
+        List<CategoriaDTO> categorias = categoriaDAO.BuscarTodosOsDTO();
+        // Mapeia nome da categoria → cor
+        Map<String, String> coresPorCategoria = categorias.stream()
+                .collect(Collectors.toMap(CategoriaDTO::getNome, CategoriaDTO::getCor));
+        Map<String, Integer> dados = categoriaDAO.RecuperarQuantidadeCompeticoesPorCategoria();
+
+        XYChart.Series<String, Integer> series = new XYChart.Series<>();
+
+        chartCategorias.setLegendVisible(false);
+
+        for (Map.Entry<String, Integer> dadosItem : dados.entrySet()) {
+            String nomeCategoria = dadosItem.getKey();
+            Integer quantidade = dadosItem.getValue();
+            XYChart.Data<String, Integer> data = new XYChart.Data<>(nomeCategoria, quantidade);
+            series.getData().add(data);
+        }
+
+        chartCategorias.getData().clear();
+        chartCategorias.getData().add(series);
+
+        // Aguarda o gráfico renderizar para aplicar os estilos (via Node)
+        Platform.runLater(() -> {
+            for (XYChart.Data<String, Integer> data : series.getData()) {
+                String nomeCategoria = data.getXValue();
+                String cor = coresPorCategoria.get(nomeCategoria);
+
+                if (cor != null && data.getNode() != null) {
+                    // Aplica a cor usando CSS inline
+                    data.getNode().setStyle("-fx-bar-fill: " + cor + ";");
+                }
+            }
+        });
     }
 
     @FXML
